89ffdbda89dee978dc25ad63bf7a36d7e9f58434
emailman <emailman@dtcc.edu>
2017-10-31 13:53:09 -0400

    Add project files.

---
+++ CIS209-Multi-Level_Sort/GraduationGroup.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace CIS209_Multi_Level_Sort
+{
+    class GraduationGroup : IComparable<GraduationGroup>
+    {
+        public string campus;
+        public string department;
+        public string degree;
+
+        public GraduationGroup(string camp, string dept, string deg)
+        {
+            campus = camp;
+            department = dept;
+            degree = deg;
+        }
+
+        public int CompareTo(GraduationGroup o2)
+        {
+            int value1 = campus.CompareTo(o2.campus);
+            if (value1 == 0)
+            {
+                int value2 = department.CompareTo(o2.department);
+                if (value2 == 0)
+                    return degree.CompareTo(o2.degree);
+                else
+                    return value2;
+            }
+            return value1;
+        }
+    }
+}
---
+++ CIS209-Multi-Level_Sort/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CIS209_Multi_Level_Sort
+{  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<GraduationGroup> graduationList = new List<GraduationGroup>();
+            graduationList.Add(new GraduationGroup("George", "CT", "CIS"));
+            graduationList.Add(new GraduationGroup("Terry", "CT", "CIS"));
+            graduationList.Add(new GraduationGroup("Owens", "CT", "CIS"));
+            graduationList.Add(new GraduationGroup("George", "CT", "CSC"));
+            graduationList.Add(new GraduationGroup("George", "CT", "CNE"));
+            graduationList.Add(new GraduationGroup("Terry", "CT", "CNE"));
+            graduationList.Add(new GraduationGroup("Owens", "CT", "CNE"));
+            graduationList.Add(new GraduationGroup("Owens", "BUS", "MGT"));
+            graduationList.Add(new GraduationGroup("Terry", "BUS", "MGT"));
+            graduationList.Add(new GraduationGroup("George", "BUS", "MGT"));
+            graduationList.Add(new GraduationGroup("Owens", "BUS", "ENT"));
+            graduationList.Add(new GraduationGroup("Terry", "BUS", "ENT"));
+            graduationList.Add(new GraduationGroup("George", "BUS", "ENT"));
+
+            // Apply a custom sort to the list
+            graduationList.Sort();
+            
+            Console.WriteLine("\n  DTCC Groups for Graduation\n");
+            Console.WriteLine("Campus\tDepartment\tDegree\n");
+            foreach (GraduationGroup each in graduationList)
+                Console.WriteLine(each.campus + "\t" + each.department + "\t\t" + each.degree);
+        }
+    }
+}
+
+    
+
